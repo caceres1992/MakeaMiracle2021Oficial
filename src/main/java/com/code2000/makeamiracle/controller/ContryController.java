6a5506fd1e5b0be9ba2f6dc7588532912d5f27ff
caceres1992 <caceresvillar_1992@outlook.com>
2021-08-08 19:34:35 -0500

    agregando valores en el properties

--- src/main/java/com/code2000/makeamiracle/controller/ContryController.java
+++ src/main/java/com/code2000/makeamiracle/controller/ContryController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api")
 @CrossOrigin
 public class ContryController {
 
