bc18dfbf74d1e4bf7c8479ece5bfa89299ad0ddb
caceres1992 <caceresvillar_1992@outlook.com>
2021-08-14 23:38:57 -0500

    arreglando CRosOrigin

--- src/main/java/com/code2000/makeamiracle/controller/DistrictController.java
+++ src/main/java/com/code2000/makeamiracle/controller/DistrictController.java
@@ -4,6 +4,7 @@ import com.code2000.makeamiracle.model.District;
 import com.code2000.makeamiracle.model.TypeDocument;
 import com.code2000.makeamiracle.service.DistrictService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.Collection;
 
 @RestController
 @RequestMapping("/district")
+@CrossOrigin
 public class DistrictController {
 
 
--- src/main/java/com/code2000/makeamiracle/controller/TypeDocumentController.java
+++ src/main/java/com/code2000/makeamiracle/controller/TypeDocumentController.java
@@ -4,6 +4,7 @@ package com.code2000.makeamiracle.controller;
 import com.code2000.makeamiracle.model.TypeDocument;
 import com.code2000.makeamiracle.service.TypeDocumentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.Collection;
 
 @RestController
 @RequestMapping("/dtype")
+@CrossOrigin
 public class TypeDocumentController {
 
 
